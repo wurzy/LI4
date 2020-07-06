e98f30c0ee9fe9fff2bbc8bf8db9849f3722affb
Válter <47797461+wurzy@users.noreply.github.com>
2020-07-06 03:34:46 +0100

    Initial commit

---
+++ Frontend/SOSAnimalUI/SOSAnimalUI.Android/obj/Debug/90/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data Hash: 825820036B5DB41E */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	6690
+	/* entry-length */
+	.word	266
+	/* value-offset */
+	.word	121
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 1779541
+	.include	"typemap.jm.inc"
---
+++ Frontend/SOSAnimalUI/SOSAnimalUI.Android/obj/Debug/90/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data Hash: 825820036B5DB41E */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	6690
+	/* entry-length */
+	.long	266
+	/* value-offset */
+	.long	121
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 1779541
+	.include	"typemap.jm.inc"
